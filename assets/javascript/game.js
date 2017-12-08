e32b80344b286b23a94190c3c5b917a76908327e
Cecily Grossmann <grssmnn2@gmail.com>
2017-12-07 23:13:39 -0600

    initial set up html

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// make computer choose random number from 19-120
+var computerCount= (Math.floor(Math.random() * 102) + 19);
+// make each crystal image a random number from 1-12
+var purple=(Math.floor(Math.random() * 12) + 1);
+var blue=(Math.floor(Math.random() * 12) + 1);
+var green=(Math.floor(Math.random() * 12) + 1);
+var white=(Math.floor(Math.random() * 12) + 1);
+
+
+// make computer pick random number and store in variable + push to computer choice div
+
+// make each crystal image/button equal to random number and store numbers
+
+// all of this should be in reset function which resets after each game is won/lost
+
+// make each image push number tied to it to the user score div
