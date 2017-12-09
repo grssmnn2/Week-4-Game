92118d0ecbac6247bd0065985b30edbf21fe0fe5
Cecily Grossmann <grssmnn2@gmail.com>
2017-12-09 00:34:28 -0600

    moving around JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,64 @@
+$(document).ready(function(){
+// call reset function at the beginning/end of each game
+reset();
+
+function reset(){
 // make computer choose random number from 19-120
-var computerCount= (Math.floor(Math.random() * 102) + 19);
+var computerCount= (Math.floor(Math.random() * 101) + 19);
 // make each crystal image a random number from 1-12
-var purple=(Math.floor(Math.random() * 12) + 1);
-var blue=(Math.floor(Math.random() * 12) + 1);
-var green=(Math.floor(Math.random() * 12) + 1);
-var white=(Math.floor(Math.random() * 12) + 1);
-
-
-// make computer pick random number and store in variable + push to computer choice div
-
-// make each crystal image/button equal to random number and store numbers
-
-// all of this should be in reset function which resets after each game is won/lost
+var purple = (Math.floor(Math.random() * 11) + 1);
+var blue = (Math.floor(Math.random() * 11) + 1);
+var green = (Math.floor(Math.random() * 11) + 1);
+var white = (Math.floor(Math.random() * 11) + 1);
+var userScore=0;
+}
+// initial wins and losses are 0, this does not reset
+var wins=0;
+var losses = 0;
 
 // make each image push number tied to it to the user score div
+
+$(".purplecrystal").on("click", function(){
+    $(".purplecrystal").attr("value");
+    userScore = userScore + purple;
+    console.log(userScore);
+    $("#playerScoreDiv").innerHTML("<p>Your Total Score is: </p>" + userScore);
+ 
+
+});
+
+$(".bluecrystal").on("click", function(){
+    $(".bluecrystal").attr("value");
+    userScore= userScore + blue;
+    console.log(userScore);
+    $("#playerScoreDiv").innerHTML("<p>Your Total Score is: </p>" + userScore);
+
+    
+ 
+});
+
+$(".greencrystal").on("click", function(){
+    $(".greencrystal").attr(green);
+    userScore= userScore + green;
+    $("#playerScoreDiv").innerHTML("<p>Your Total Score is: </p>" + userScore);
+   
+
+});
+
+$(".whitecrystal").on("click", function(){
+    $(".whitecrystal").attr(white);
+    userScore = userScore + white;
+    $("#playerScoreDiv").innerHTML("<p>Your Total Score is: </p>" + userScore); 
+
+});
+
+
+
+
+
+
+
+
+
+
+});
